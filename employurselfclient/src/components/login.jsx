e028e4184d498eef02979d4a572c0e3aadc56693
Lorenz <CallMeEnte@web.de>
2021-03-18 14:48:30 +0100

    Created Example Component

---
+++ employurselfclient/src/components/login.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+export default function LoginPage(props){
+    const [aktuellerWert, setAktuellerWert] = useState('');
+
+    return(
+        <h2>Das ist eine LoginPage!</h2>
+    );
+}
